951d4d96d7a73683e2c07856301b4abcaeed43a4
hedo95 <44504802+hedo95@users.noreply.github.com>
2019-11-11 22:01:44 +0100

    Update EgaraBO.dart

--- Logic/BO/EgaraBO.dart
+++ Logic/BO/EgaraBO.dart
@@ -1,6 +1,49 @@
-import 'dart:convert';
+mport 'dart:convert';
 import 'dart:ffi';
 import 'dart:io' show File;
 import 'dart:convert' show json;
 import 'dart:io';
+import 'package:egarafutsal/Logic/Model/Player.dart';
+import 'package:egarafutsal/Logic/DAO/EgaraDAO.dart';
+
+int getId(var data)
+{
+  return data[data.length-1].id + 1;
+}
+
+Player createUpdatePlayer({int id, Player player})
+{
+  if (id == null && player == null)
+  {
+    return createUpdatePlayer(player: new Player.def());
+  }
+  else if(id != null && player == null)
+  {
+    try
+    {
+      Player foundPlayer = getAllPlayersData().firstWhere((item) => item.id == id);
+      return createUpdatePlayer(player: foundPlayer);
+    }
+    catch (Exception)
+    {
+      return Exception('No se ha encontrado el modelo con id: ' + id.toString() + '\n');
+    }
+  }
+  else 
+  {
+    var data = getAllPlayersData();
+    if (data.any((item) => item.id == player.id))
+    {
+      int index = data.indexWhere((item) => item.id == player.id);
+      data.removeAt(index);
+      data.add(player);
+      exportPlayersData(data);
+    }
+    else
+    {
+      data.add(player);
+      exportPlayersData(data);
+    }
+  }
+}
 
