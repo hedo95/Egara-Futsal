4d5f8b7ef9372053618bad27234cdc6d8c70ff07
hedo95 <44504802+hedo95@users.noreply.github.com>
2019-11-12 12:54:10 +0100

    Update EgaraDAO.dart

--- Logic/DAO/EgaraDAO.dart
+++ Logic/DAO/EgaraDAO.dart
@@ -4,15 +4,24 @@ import 'dart:io' show File;
 import 'dart:convert' show json;
 import 'dart:io';
 import 'package:egarafutsal/Logic/Model/Player.dart';
+import 'package:egarafutsal/Logic/Model/Team.dart';
 
 String path = '/Users/jesushedo/Flutter/egarafutsal/Data/',
-       playersfile = path + 'Players.json';
+       playersfile = path + 'Players.json',
+       teamsfile = path + 'Teams.json';
 
 List<Player> getAllPlayersData()
 {
   var jsonString = File(playersfile).readAsStringSync();
   List jsonData = json.decode(jsonString);
   return new List<Player>.from(jsonData.map((item) => new Player.fromJson(item)).toList());
+}
+
+List<Team> getAllTeamsData()
+{
+  var jsonString = File(teamsfile).readAsStringSync();
+  List jsonData = json.decode(jsonString);
+  return new List<Team>.from(jsonData.map((item) => new Team.fromJson(item)).toList());
 }
 
 void exportPlayersData(List<Player> data)
@@ -23,6 +32,14 @@ void exportPlayersData(List<Player> data)
   File(playersfile).writeAsStringSync(jsonData.toString());
 }
 
+void exportTeamsData(List<Team> data)
+{
+  data.sort((a,b) => a.id.compareTo(b.id));
+  List<dynamic> jsonData = [];
+  data.forEach((item) => jsonData.add(json.encode(item.toJson())));
+  File(teamsfile).writeAsStringSync(jsonData.toString());
+}
+
 void appendPlayer(Player player)
 {
   var data = getAllPlayersData();
@@ -30,6 +47,16 @@ void appendPlayer(Player player)
   {
     data.add(player);
     exportPlayersData(data);
+  }
+}
+
+void appendTeam(Team team)
+{
+  var data = getAllTeamsData();
+  if (!data.any((item) => item.id == team.id))
+  {
+    data.add(team);
+    exportTeamsData(data);
   }
 }
 
