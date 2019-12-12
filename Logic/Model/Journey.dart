48cb4c70dc02364cb819fcdafe52751cdd193eb7
hedo95 <44504802+hedo95@users.noreply.github.com>
2019-12-12 17:31:06 +0100

    Update Journey.dart

--- Logic/Model/Journey.dart
+++ Logic/Model/Journey.dart
@@ -5,12 +5,8 @@ import 'dart:convert' show json;
 import 'dart:io';
 import 'package:egarafutsal/Logic/Model/Player.dart';
 
-class Journey
-{
-  int id, journey;
+int id, journey;
   List<Game> games;
-  List<Team> teams;
-  List<Player> players;
 
   Journey(this.games)
   {
@@ -21,15 +17,3 @@ class Journey
     }
     this.id = 4000 + (this.journey - 1); // Solo sirve con el registro de una unica temporada
   }
-
-  addTeams(List<Team> teams)
-  {
-    this.teams = teams;
-  }
-
-  addPlayers(List<Player> players)
-  {
-    this.players = players;
-  }
-
-}
