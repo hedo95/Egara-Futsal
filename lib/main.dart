28a0c4ba491efdeb619455f2d31f7cc1ab03f67c
Jesus Hedo Garcia <jesushedo@MacBook-Pro-de-Jesus.local>
2019-12-20 21:49:37 +0100

    Firebase para iOS añadido.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:egaradefinitiu/screens/Clasificacion.dart';
 import 'package:egaradefinitiu/style/Theme.dart';
 import 'package:egaradefinitiu/subscreens/Graficos_jugadoras.dart';
@@ -30,3 +31,47 @@ class MyApp extends StatelessWidget {
   }
 }
 
+*/
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'logic/Logic/DAO/FirebaseContext.dart';
+import 'logic/Logic/Model/Game.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.add),
+                color: Colors.blue[500],
+                onPressed: ()
+                {
+                  var db = new FirebaseContext();
+                  db.loadGames().then((list){
+                    list.sort((a,b) => a.id.compareTo(b.id));
+                    list.forEach((item) => print(item.id.toString()));
+                  });
+                }
+              ),
+              Text('hola Mundo',
+              )
+            ]
+          )
+        ),
+      ),
+    );
+  }
+}
