5780f05daab337f1517e8530ceca4237540856f9
Jesus Hedo Garcia <jesushedo@MacBook-Pro-de-Jesus.local>
2019-12-21 14:54:44 +0100

    Provider modelo + Ficheros para firebase Android.

---
+++ lib/logic/Logic/Model/Provider.dart
@@ -0,0 +1,25 @@
+import '../BO/EgaraBO.dart';
+import '../DAO/EgaraDAO.dart';
+import 'Player.dart';
+import 'Team.dart';
+import 'Game.dart';
+
+class Provider
+{
+  List<Game> games;
+  List<Team> teams;
+  List<Player> players;
+
+  Provider.games(this.games){
+    this.games.sort((a,b) => a.id.compareTo(b.id));
+  }
+  
+  Provider.players(this.players){
+    this.players.sort((a,b) => a.idteam.compareTo(b.idteam));
+  }
+
+  Provider.teams(this.teams){
+    this.teams.sort((a,b) => a.id.compareTo(b.id));
+  }
+
+}
