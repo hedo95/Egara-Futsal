d7ac96e024b8f0293b3a001c0031c38686b2677a
Jesus Hedo Garcia <jesushedo@MacBook-Pro-de-Jesus.local>
2019-12-26 14:54:29 +0100

    Handling error on void main()

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,14 @@ import 'logic/Logic/Providers/PlayerProvider.dart';
 
 
 
-void main() => runApp(MyApp());
+void main(){ 
+  FlutterError.onError = (FlutterErrorDetails details){
+    FlutterError.dumpErrorToConsole(details);
+    if (kReleaseMode)
+      exit(1);
+  };
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   get bottomNavBarIndex => null;
@@ -84,7 +91,14 @@ import 'logic/Logic/Model/Game.dart';
 import 'logic/Logic/Model/Team.dart';
 
 
-void main() => runApp(MyApp());
+void main(){ 
+  FlutterError.onError = (FlutterErrorDetails details){
+    FlutterError.dumpErrorToConsole(details);
+    if (kReleaseMode)
+      exit(1);
+  };
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -124,6 +138,7 @@ class MyApp extends StatelessWidget {
 
 */
 
+import 'dart:async';
 import 'package:egaradefinitiu/logic/Logic/DAO/FirebaseContext.dart';
 import 'package:egaradefinitiu/logic/Logic/Model/Game.dart';
 import 'package:egaradefinitiu/logic/Logic/Model/Team.dart';
@@ -136,11 +151,20 @@ import 'package:provider/provider.dart';
 import 'package:egaradefinitiu/logic/Logic/Providers/GameProvider.dart';
 import 'package:egaradefinitiu/logic/Logic/Providers/TeamProvider.dart';
 import 'package:egaradefinitiu/logic/Logic/Providers/PlayerProvider.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:sentry/sentry.dart';
 
 
-
-void main() => runApp(MyApp());
-
+void main() {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.dumpErrorToConsole(details);
+    if (kReleaseMode)
+      exit(1);
+  };
+  runApp(MyApp());
+}
+  
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
@@ -159,7 +183,7 @@ class _MyAppState extends State<MyApp> {
               stream: db.loadTeams(),
               builder: (context, snapshot2) {
                 if(!snapshot1.hasData || !snapshot2.hasData){
-                  return Center( child: CircularProgressIndicator());
+                  return Center(child: CircularProgressIndicator());
                 }
                 else if(snapshot1.hasError){
                   return Center(child: Text(snapshot1.error));
@@ -167,7 +191,7 @@ class _MyAppState extends State<MyApp> {
                   return Center(child: Text(snapshot2.error));
                 }else{
                   ChangeNotifierProvider provider1 = ChangeNotifierProvider<GameProvider>.value(value: GameProvider(snapshot1.data)); //(create: (context) => GameProvider(snapshot1.data)); // Los metemos en su provider
-                  ChangeNotifierProvider provider2 = ChangeNotifierProvider<TeamProvider>(create: (context) => TeamProvider(snapshot2.data)); // Lo metemos en su provider
+                  ChangeNotifierProvider provider2 = ChangeNotifierProvider<TeamProvider>.value(value: TeamProvider(snapshot2.data)); 
                   ChangeNotifierProvider provider3 = ChangeNotifierProvider<PlayerProvider>.value(value: PlayerProvider(snapshot1.data)); //(create: (context) => PlayerProvider(snapshot1.data)); // Provider3 es de jugadores, y no cargamos nada porque los sacamos de todos los partidos, en el constuctor ya llamamos una función del EgaraBO que nos saca todos los distintos jugadores de los partidos.
                   return MultiProvider(
                     providers: [provider1, provider2, provider3], // Metemos los 3 providers en un Multiproviders
